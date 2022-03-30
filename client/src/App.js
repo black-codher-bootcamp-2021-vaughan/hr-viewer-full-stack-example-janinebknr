4e8bb2cfabe20ec4bf2138e7be9b6e75af6bb898
janinebknr <87192949+janinebknr@users.noreply.github.com>
2022-03-30 20:50:17 +0100

    adding new component to edit person UpdatePersonInfo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import PersonViewer from "./components/PeopleViewer";
 import PersonDetails from "./components/PersonDetails";
+import UpdatePersonInfo from "./components/UpdatePersonInfo";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<PersonViewer />} />
         <Route path="/person-details/:id" element={<PersonDetails />} />
+        <Route path="/edit-person/:id" element={<UpdatePersonInfo />} />
         {/* 
           <Route path='/create-person' element={CreateBook} />
           <Route path='/edit-person/:id' element={UpdateBookInfo} />
---
+++ client/src/components/UpdatePersonInfo.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useCallback } from "react";
+import "../App.css";
+
+const UpdatePersonInfo = () => {
+  console.log("Test");
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default UpdatePersonInfo;
